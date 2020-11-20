7a435e8b49c89136af9fc7e66502803bfcf22bbd
andresgfranco <andresgfranco0@gmail.com>
2020-11-20 16:26:43 +0000

    Organizing the project in files and folders

--- mini_shell.c
+++ mini_shell.c
@@ -1,53 +1,55 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/wait.h>
-#define GREEN_T "\x1b[32m"
-#define RESET_COLOR "\x1b[37m"
+#include "header_minishell.h"
 
-#define DELIM " ,!¡¿?\'\"\n\t"
 char *_strcat(char *dest, char *src);
+
+/**
+  * main - main function where we receive the arguments
+  * through the CLI
+  * @ac: number of arguments
+  * @av: arguments
+  * @envp: environment pointer
+  * Return: 0 if success
+**/
 int main(int ac, char **av, char **envp)
 {
-        char *prompt = "hola@shell$ ", *BUFFER;
+        char *prompt = "campo_gonzalez_shell$ ", *buffer,
+	     *token = NULL, *token2[1024], path[60], path2[20];
         size_t bufsize = 1024;
-        pid_t child_pid;
-        char *token = NULL, *token2[1024];
         int reset, i;
         unsigned int getln;
+	pid_t child_pid;
 
         while (1)
         {
                 i = 0;
                 reset = 0;
+                path = "/bin/";
+                path2 = "/sbin/";
 
-                char path[60] = "/bin/";
-                char path2[20] = "/sbin/";
                 if (isatty(STDOUT_FILENO) == 1)
                         printf(GREEN_T "%s" RESET_COLOR, prompt);
 
-                getln = getline(&BUFFER, &bufsize, stdin);
+                getln = getline(&buffer, &bufsize, stdin);
                 if (getln == EOF)
                 {
-                        printf("exit\n");
-                        printf("\n");
+                        _puts("exit\n");
+                        _puts("\n");
                         exit(EXIT_SUCCESS);
                 }
-
-                token = strtok(BUFFER, DELIM);
+		
+                token = strtok(buffer, DELIM);
                 while (token != NULL)
                 {
                         token2[i] = token;
                         token = strtok(NULL, DELIM);
                         i++;
                 }
+
                 if (token2[0] != NULL)
                 {
                         if(strcmp(token2[0], "exit") == 0)
                         {
-                                printf("\n-"); //PRODUCE DOS SALTOS DE LINEA
+                                _puts("\n-"); //PRODUCE DOS SALTOS DE LINEA
                                 exit(0);
                         }
                 }
@@ -59,8 +61,8 @@ int main(int ac, char **av, char **envp)
                 }
                 if (child_pid == 0)
                 {
-                        execve(_strcat(path, token2[0]), token2, NULL);
-                        execve(_strcat(path2, token2[0]), token2, NULL);
+			execve(_strcat(path, token2[0]), token2, NULL);
+			execve(_strcat(path2, token2[0]), token2, NULL);
 
                         exit(0);
                 }
