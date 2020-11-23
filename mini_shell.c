1a931cae3650491e769c61c8028150a622ff6998
andresgfranco <andresgfranco0@gmail.com>
2020-11-23 16:13:25 +0000

    Project organized in files

--- mini_shell.c
+++ mini_shell.c
@@ -1,91 +1,70 @@
 #include "header_minishell.h"
-
-char *_strcat(char *dest, char *src);
-
 /**
-  * main - main function where we receive the arguments
-  * through the CLI
+  * main - main function of our shell that will
+  * receive the arguments in an infinite loop
   * @ac: number of arguments
   * @av: arguments
-  * @envp: environment pointer
+  * @envp: environment variables
   * Return: 0 if success
 **/
+
 int main(int ac, char **av, char **envp)
 {
-        char *prompt = "campo_gonzalez_shell$ ", *buffer,
-	     *token = NULL, *token2[1024], path[60], path2[20];
-        size_t bufsize = 1024;
-        int reset, i;
-        unsigned int getln;
+	char *prompt = "hola@shell$ ", *BUFFER;
+	size_t bufsize = 1024;
 	pid_t child_pid;
+	char *token = NULL, *token2[1024];
+	int reset, i, b;
+	unsigned int getln;
+	char **mi_envp, *mypath, path[60] = "/bin/", path2[60] = "/sbin/", *path_str;
 
-        while (1)
-        {
-                i = 0;
-                reset = 0;
-                path = "/bin/";
-                path2 = "/sbin/";
-
-                if (isatty(STDOUT_FILENO) == 1)
-                        printf(GREEN_T "%s" RESET_COLOR, prompt);
-
-                getln = getline(&buffer, &bufsize, stdin);
-                if (getln == EOF)
-                {
-                        _puts("exit\n");
-                        _puts("\n");
-                        exit(EXIT_SUCCESS);
-                }
-		
-                token = strtok(buffer, DELIM);
-                while (token != NULL)
-                {
-                        token2[i] = token;
-                        token = strtok(NULL, DELIM);
-                        i++;
-                }
-
-                if (token2[0] != NULL)
-                {
-                        if(strcmp(token2[0], "exit") == 0)
-                        {
-                                _puts("\n-"); //PRODUCE DOS SALTOS DE LINEA
-                                exit(0);
-                        }
-                }
-                child_pid = fork();
-                if (child_pid == -1)
-                {
-                        perror("Error");
-                        return (1);
-                }
-                if (child_pid == 0)
-                {
+	while (1)
+	{
+		i = 0;
+		reset = 0;		
+		if (isatty(STDOUT_FILENO) == 1)
+			printf(GREEN_T "%s" RESET_COLOR, prompt);		
+		getln = getline(&BUFFER, &bufsize, stdin);
+		if (getln == EOF)
+		{
+			printf("exit\n");
+			printf("\n");
+			exit(EXIT_SUCCESS);
+		}
+		token = strtok(BUFFER, DELIM);
+		//newenviron();
+		mi_envp = _getpath("PATH");
+		mypath = _getenv("PAT");
+		while (token != NULL)
+		{
+			token2[i] = token;
+			token = strtok(NULL, DELIM);
+			i++;
+		}
+		if (token2[0] != NULL)
+		{
+			if(strcmp(token2[0], "exit") == 0)
+			{
+				printf("\n-"); 
+				exit(0);
+			}
+		}
+		child_pid = fork();
+		if (child_pid == -1)
+		{
+			perror("Error");
+			return (1);
+		}
+		if (child_pid == 0)
+		{
 			execve(_strcat(path, token2[0]), token2, NULL);
 			execve(_strcat(path2, token2[0]), token2, NULL);
-
-                        exit(0);
-                }
-                else
-                        child_pid = wait(NULL);
-                for (;reset <= i; reset ++)
-                        token2[reset] = NULL;
-        }
-        return (0);
-}
-
-char *_strcat(char *dest, char *src)
-{
-        int a, b;
-
-        for (a = 0; dest[a] != '\0'; a++)
-        {
-        }
-        for (b = 0; src[b] != '\0'; b++)
-        {
-        dest[a] = src[b];
-        a++;
-        }
-        dest[a] = '\0';
-        return (dest);
+			exit(0);
+		}
+		else
+			child_pid = wait(NULL);
+		for (;reset <= i; reset ++)
+			token2[reset] = NULL;
+	}
+	return (0);
 }
